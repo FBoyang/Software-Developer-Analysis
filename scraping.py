abc36408dd6b9a74296af36cf34cc687efd03511
FBoyang <fbyang1995@gmail.com>
2018-02-03 17:47:38 -0500

    can run

--- scraping.py
+++ scraping.py
@@ -8,15 +8,19 @@ from urllib.request import Request, urlopen
 driver = webdriver.Chrome() #Can be changed
 
 #url2 = 'https://www.linkedin.com/jobs/search/?keywords=Software%20Developer&location=Worldwide&locationId=OTHERS.worldwide'
-url ='https://www.google.com/search?client=ubuntu&hs=Y4q&channel=fs&ei=zeNtWvuuDIW2zwKK45-IDA&q=linkedin+software+developer+jobs&oq=linkedin+software+developer+jo&gs_l=psy-ab.1.0.0j0i22i30k1l3.7626.12395.0.14246.13.13.0.0.0.0.169.1500.5j8.13.0....0...1c.1.64.psy-ab..0.13.1493...0i67k1.0.pdDGaoWhWPI&ibp=htl;jobs&sa=X&ved=0ahUKEwiI57bR9PrYAhXE2lMKHVIsAxQQiYsCCFUoAQ#fpstate=tldetail&htidocid=cL_9ZSLGsMxLkEJbAAAAAA%3D%3D&htivrt=jobs'
+url ='https://www.glassdoor.com/Job/software-developer-jobs-SRCH_KO0,18.htm'
 driver.get(url)
-time.sleep(5)
+time.sleep(2)
 infor = 0
 
 #create a new file in the current directory called job.txt
 f = open('job.txt', 'w')
 
+driver.find_element_by_link_text(2).click()
+time.sleep(3)
+driver.find_element_by_xpath("//button[@type='button' and @class='mfp-close']").click()
 #this is the function to scroll down the screen, you can try to adjust the sleeping time if too slow, time is the total scroll time
+'''
 def execute_times(times):
     for i in range(2, times + 1):
         driver.find_element_by_link_text(str(i)).click()
@@ -40,3 +44,4 @@ f.close()
 #webpage = urlopen(req).read()
 
 #print(webpage)
+'''
