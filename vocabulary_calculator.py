d62b4fe67bbf5f8648d2e82feed681d73c2bcced
Boyang Fu <fbyang1995@gmail.com>
2018-02-07 16:42:37 -0500

    Update vocabulary_calculator.py

--- vocabulary_calculator.py
+++ vocabulary_calculator.py
@@ -1,1 +1,21 @@
 
+#######################################
+#Boyang's part
+key_word_dict = {}
+
+job_dis_file = open("job.txt", 'r', encoding = 'utf8')
+while True:
+    job_dis = job_dis_file.readline()
+    if (job_dis == ""):
+        break
+    else:
+        for word in job_dis.split():
+            if not word in job_dis_file:
+                #if the key word not in dictionary, add it 
+                key_word_dict.update({word: 1})
+            else:
+                #if the key word in the dictionary, increment it
+                job_dis_file[word] += 1
+                
+
+            
